ee3ac732c2a462990b11b5fb32d16bfae2475aad
Alexander Krivonosov <akrivonosov@provectus.com>
2022-02-17 20:16:52 +0300

    Add logout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,21 @@
 import { Box } from '@mui/system';
 import React from 'react';
 import * as colors from '@mui/material/colors';
-import { AppBar, Link, Toolbar, Typography } from '@mui/material';
+import { AppBar, IconButton, Link, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
+import { useAuth0 } from '@auth0/auth0-react';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 const Layout: React.FC = ({children}) => {
+  const {isAuthenticated, logout} = useAuth0();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Box
       sx={{
@@ -31,6 +43,37 @@ const Layout: React.FC = ({children}) => {
           >
             Hot Ekspresso Travel ✈️
           </Link>
+          {isAuthenticated &&
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={() => logout()}>Logout</MenuItem>
+              </Menu>
+            </div>
+          }
         </Toolbar>
       </AppBar>
         {children}
