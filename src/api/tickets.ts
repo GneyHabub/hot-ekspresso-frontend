a9c13c9cc8c7f519f73a4b1e068e339c5d5bf501
Alexander Krivonosov <akrivonosov@provectus.com>
2022-03-14 20:13:05 +0300

    Update tickets.ts

--- src/api/tickets.ts
+++ src/api/tickets.ts
@@ -1,46 +1,13 @@
-import dayjs from "dayjs";
+import axios from "axios";
+import getEnv from "../utils/getEnv";
 import { Airport, Booking } from "../utils/types";
 
 export async function fetchBookings(authToken: string): Promise<Booking[]> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve([
-        {
-          id: "AAA",
-          price: 300,
-          flights: [{
-            from: "KZN", 
-            to: "GSV", 
-            departure: dayjs().startOf("month").toDate(), 
-            arrival: dayjs().startOf("month").add(1, "hour").toDate(),
-            carrier: "RedWings"
-          }],
-          serviceClass: "Economy",
-        },
-        {
-          id: "BBB",
-          price: 700,
-          flights: [
-            {
-              from: "GSV", 
-              to: "KZN", 
-              departure: dayjs().startOf("month").add(10, "day").toDate(), 
-              arrival: dayjs().startOf("month").add(10, "day").add(1, "hour").toDate(),
-              carrier: "RedWings"
-            },
-            {
-              from: "KZN", 
-              to: "DME", 
-              departure: dayjs().startOf("month").add(10, "day").add(3, "hour").toDate(), 
-              arrival: dayjs().startOf("month").add(10, "day").add(4, "hour").toDate(),
-              carrier: "Pobeda"
-            }
-          ],
-          serviceClass: "Business",
-        }
-    ]);
-    }, 300);
-  });;
+  const {data} = await axios.get(`${getEnv("BACKEND_URL")}/proxy/attractions/types`,
+  {
+    headers: { Authorization: `Bearer ${authToken}` },
+  });
+  return data;
 }
 
 export async function fetchAirports(authToken: string): Promise<Airport[]> {
