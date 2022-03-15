92abd71bc481d38b481ad5b3f6010eddd4558e31
Alexander Krivonosov <akrivonosov@provectus.com>
2022-03-15 10:30:09 +0300

    Test notification

---
+++ src/components/tests/Notification.spec.tsx
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import {Notification} from'../Notification/Notification';
+describe('Notification', () => {
+  describe("error notification", () => {
+    it('matches the snapshot', () => {
+      const wrapper = render(<BrowserRouter><Notification type="error" message="ecdcads" header="hdbvhjzdg"/></BrowserRouter>);
+      expect(wrapper.baseElement).toMatchSnapshot();
+    });
+  });
+
+  describe("success notification", () => {
+    it('matches the snapshot', () => {
+      const wrapper = render(<BrowserRouter><Notification type="success"/></BrowserRouter>);
+      expect(wrapper.baseElement).toMatchSnapshot();
+    });
+  });
+})
