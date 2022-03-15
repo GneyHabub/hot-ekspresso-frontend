fa6eb85b4bd4c81f54c72b71f5fb34270e6ec4b9
Alexander Krivonosov <akrivonosov@provectus.com>
2022-03-15 09:49:51 +0300

    tests

--- src/api/tickets.ts
+++ src/api/tickets.ts
@@ -4,51 +4,51 @@ import getEnv from "../utils/getEnv";
 import { Airport, Booking } from "../utils/types";
 
 export async function fetchBookings(authToken: string): Promise<Booking[]> {
-  const {data} = await axios.get(`${getEnv("BACKEND_URL")}/proxy/attractions/all`,
-  {
-    headers: { Authorization: `Bearer ${authToken}` },
-  });
-  console.log(data);
-  return data;
-  // return new Promise((resolve, reject) => {
-  //   setTimeout(() => {
-  //     resolve([
-  //       {
-  //         id: "AAA",
-  //         price: 300,
-  //         flights: [{
-  //           from: "KZN", 
-  //           to: "GSV", 
-  //           departure: dayjs().startOf("month").toDate(), 
-  //           arrival: dayjs().startOf("month").add(1, "hour").toDate(),
-  //           carrier: "RedWings"
-  //         }],
-  //         serviceClass: "Economy",
-  //       },
-  //       {
-  //         id: "BBB",
-  //         price: 700,
-  //         flights: [
-  //           {
-  //             from: "GSV", 
-  //             to: "KZN", 
-  //             departure: dayjs().startOf("month").add(10, "day").toDate(), 
-  //             arrival: dayjs().startOf("month").add(10, "day").add(1, "hour").toDate(),
-  //             carrier: "RedWings"
-  //           },
-  //           {
-  //             from: "KZN", 
-  //             to: "DME", 
-  //             departure: dayjs().startOf("month").add(10, "day").add(3, "hour").toDate(), 
-  //             arrival: dayjs().startOf("month").add(10, "day").add(4, "hour").toDate(),
-  //             carrier: "Pobeda"
-  //           }
-  //         ],
-  //         serviceClass: "Business",
-  //       }
-  //   ]);
-  //   }, 300);
+  // const {data} = await axios.get(`${getEnv("BACKEND_URL")}/proxy/attractions/all`,
+  // {
+  //   headers: { Authorization: `Bearer ${authToken}` },
   // });
+  // console.log(data);
+  // return data;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        {
+          id: "AAA",
+          price: 300,
+          flights: [{
+            from: "KZN", 
+            to: "GSV", 
+            departure: dayjs().startOf("month").toDate(), 
+            arrival: dayjs().startOf("month").add(1, "hour").toDate(),
+            carrier: "RedWings"
+          }],
+          serviceClass: "Economy",
+        },
+        {
+          id: "BBB",
+          price: 700,
+          flights: [
+            {
+              from: "GSV", 
+              to: "KZN", 
+              departure: dayjs().startOf("month").add(10, "day").toDate(), 
+              arrival: dayjs().startOf("month").add(10, "day").add(1, "hour").toDate(),
+              carrier: "RedWings"
+            },
+            {
+              from: "KZN", 
+              to: "DME", 
+              departure: dayjs().startOf("month").add(10, "day").add(3, "hour").toDate(), 
+              arrival: dayjs().startOf("month").add(10, "day").add(4, "hour").toDate(),
+              carrier: "Pobeda"
+            }
+          ],
+          serviceClass: "Business",
+        }
+    ]);
+    }, 300);
+  });
 }
 
 export async function fetchAirports(authToken: string): Promise<Airport[]> {
@@ -78,4 +78,8 @@ export async function fetchAirports(authToken: string): Promise<Airport[]> {
     ]);
     }, 300);
   });;
+}
+
+export async function creaeBooking(authToken: string): Promise<void> {
+  
 }
