5327e2d00ac6500a5553809e97d76d2f9edf2aef
Alexander Krivonosov <akrivonosov@provectus.com>
2022-05-09 10:02:34 +0300

    add tests

--- jest/mocks/jest.setup.js
+++ jest/mocks/jest.setup.js
@@ -1,6 +1,1 @@
 import '@testing-library/jest-dom';
-
-jest.mock('../../src/utils/getEnv.ts', () => () => 'mockEnvVar');
-jest.mock('../../src/utils/hooks/useAuthToken', () => ({
-  useAuthToken: () => 'mockAuthToken',
-}));
--- src/components/tests/Layout.spec.tsx
+++ src/components/tests/Layout.spec.tsx
@@ -2,10 +2,41 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import Layout from '../Layout/Layout';
+import {
+  H1, H2, H3, H4,
+} from '../typography';
 
 describe('Login', () => {
   it('matches the snapshot', () => {
     const wrapper = render(<BrowserRouter><Layout /></BrowserRouter>);
+    expect(wrapper.baseElement).toMatchSnapshot();
+  });
+});
+
+describe('H1', () => {
+  it('matches the snapshot', () => {
+    const wrapper = render(<BrowserRouter><H1 /></BrowserRouter>);
+    expect(wrapper.baseElement).toMatchSnapshot();
+  });
+});
+
+describe('H2', () => {
+  it('matches the snapshot', () => {
+    const wrapper = render(<BrowserRouter><H2 /></BrowserRouter>);
+    expect(wrapper.baseElement).toMatchSnapshot();
+  });
+});
+
+describe('H3', () => {
+  it('matches the snapshot', () => {
+    const wrapper = render(<BrowserRouter><H3 /></BrowserRouter>);
+    expect(wrapper.baseElement).toMatchSnapshot();
+  });
+});
+
+describe('H4', () => {
+  it('matches the snapshot', () => {
+    const wrapper = render(<BrowserRouter><H4 /></BrowserRouter>);
     expect(wrapper.baseElement).toMatchSnapshot();
   });
 });
