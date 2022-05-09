d61eddbd441d916f5cc9b03fa8ae22aeee91ba71
Alexander Krivonosov <akrivonosov@provectus.com>
2022-05-09 15:55:18 +0300

    add tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,9 +1,10 @@
 /* eslint-disable jest/expect-expect */
-describe('Hot Ekspresso Test', () => {
-  it('Gets, types and asserts', () => {
+describe('Login', () => {
+  it('Logs in', () => {
     cy.visit('https://hot-ekspresso.vercel.app/');
     cy.get('#standard-adornment-username').type('admin');
     cy.get('#standard-adornment-password').type('1234');
     cy.get('#submit').click();
+    cy.location('pathname').should('eq', '/tickets');
   });
 });
