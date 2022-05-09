8ec625ea4a5a564c75a0d4089633867eb2bfe131
Alexander Krivonosov <akrivonosov@provectus.com>
2022-05-09 09:52:36 +0300

    fix tickets

--- src/pages/tickets/__tests__/NewBooking.spec.tsx
+++ src/pages/tickets/__tests__/NewBooking.spec.tsx
@@ -4,24 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { mocked } from 'ts-jest/utils';
 import { LocalizationProvider } from '@mui/lab';
 import DateAdapter from '@mui/lab/AdapterDayjs';
-import { useAuthToken } from '../../../utils/hooks/useAuthToken';
 import NewBooking from '../NewBooking';
 
-jest.mock('../../../utils/hooks/useAuthToken', () => ({
-  useAuthToken: jest.fn(),
-}));
 jest.mock('../../../store/newBookingForm.store', () => ({
   submittingStatus: 'fetched',
   airportsfetchingStatus: 'fetched',
   resetForm: jest.fn(),
   fetchAirports: jest.fn(),
 }));
-const mockedAuth = mocked(useAuthToken, true);
 
 describe('NewBooking', () => {
   describe('when authenticated', () => {
     it('matches the snapshot', () => {
-      mockedAuth.mockReturnValueOnce('ssss');
       const wrapper = render(
         <LocalizationProvider dateAdapter={DateAdapter}>
           <BrowserRouter>
@@ -35,7 +29,6 @@ describe('NewBooking', () => {
 
   describe('when not authenticated', () => {
     it('matches the snapshot', () => {
-      mockedAuth.mockReturnValueOnce('');
       const wrapper = render(
         <LocalizationProvider dateAdapter={DateAdapter}>
           <BrowserRouter>
